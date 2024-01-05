2080864697942ecf4c59a752eb65155588c1106b
Ade Shennaike <shennaiek2386@hotmail.com>
2024-01-05 07:01:25 -0500

    no sleep

---
+++ home.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const selectElement = document.getElementById('muscle-group'); // The select dropdown
+    const searchButton = document.getElementById('search-button'); // The search button
+    const axios = require('axios').default; // Make sure to include Axios
+  
+    searchButton.addEventListener('click', () => {
+        console.log("click")
+      const muscleGroup = selectElement.value;
+      const response = axios.get(`https://informed-fixness-d570fbe159e8.herokuapp.com/exercise/${muscleGroup}`)
+        .then(response => {
+          // Handle success
+          console.log(response.data);
+        })
+        .catch(error => {
+          // Handle error
+          console.error('Error fetching exercises:', error);
+        });
+    });
+  });
+  
