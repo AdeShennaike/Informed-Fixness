8b4ede8a4099f6e4fc2054c6952a74cb091ca3a4
Ade Shennaike <shennaiek2386@hotmail.com>
2024-01-05 03:29:27 -0500

    added a volume icon to toggle the music

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-//add some javascript
+// ***********************
+// Volume Button
+// *********************** 
+const video = document.getElementById('background-video');
+const volumeIcon = document.getElementById('volume-icon');
+
+// Click event listener to the volume icon
+volumeIcon.addEventListener('click', function() {
+    // Toggle the 'muted' property of the video
+    video.muted = !video.muted;
+
+    // Change the icon based on the mute state
+    volumeIcon.src = video.muted ? './assets/mute volume icon.jpg' : './assets/volume.jpg';
+});
